3a6fbb4a3be3207a082bf530dd3c48cf56c81a44
Emily Tao <emilytao@Emilys-2018-MacBook-Pro.local>
2018-11-16 12:34:09 -0800

    Updates

---
+++ precache-manifest.3d45f689fc4fd8ac3510c45e6d647401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68185b963305af256f84",
+    "url": "/ps-7-emilytao19/static/js/runtime~main.68185b96.js"
+  },
+  {
+    "revision": "42c68aef53c36e2c1773",
+    "url": "/ps-7-emilytao19/static/js/main.42c68aef.chunk.js"
+  },
+  {
+    "revision": "09b0f2f0438cfdda41fb",
+    "url": "/ps-7-emilytao19/static/js/1.09b0f2f0.chunk.js"
+  },
+  {
+    "revision": "42c68aef53c36e2c1773",
+    "url": "/ps-7-emilytao19/static/css/main.c54e0616.chunk.css"
+  },
+  {
+    "revision": "b32a3e84648b67207286e17fef9fa025",
+    "url": "/ps-7-emilytao19/index.html"
+  }
+];
