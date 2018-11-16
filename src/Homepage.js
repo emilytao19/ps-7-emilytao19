8d6a2ddc61240576c83563526dc36f641d2e9aa6
Emily Tao <emilytao@Emilys-2018-MacBook-Pro.local>
2018-11-16 12:25:58 -0800

    Finished

---
+++ src/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+    render() {
+        return(
+            <div>Welcome to my website! I hope you enjoy learning more about me.</div>
+        )
+    }
+}
+
+export {Homepage};
